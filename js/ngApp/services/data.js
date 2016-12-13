615f6c186537b9d6dad3bb40e43f9c6f18fbb180
Rodrigo Grisolía <grisoliarodrigo@gmail.com>
2016-12-13 20:21:14 -0300

    Test inicial

--- js/ngApp/services/data.js
+++ js/ngApp/services/data.js
@@ -11,12 +11,12 @@ appData.secciones = [
     {titulo: "Cultura organizacional", paginas: [{titulo: "", nombreTemplate: "culturaOrganizacional", noMostrarEnIndice: true}]},
     {titulo: "Centros y regionales", paginas: [{titulo: "", nombreTemplate: "centrosYRegionales", noMostrarEnIndice: true}]},
     {titulo: "Tecnología nuclear y medicina nuclear", paginas: [{titulo: "", nombreTemplate: "tecnologiaNuclear", noMostrarEnIndice: true}]},
-    {titulo: "Ciclo de combustible nuclear", paginas: [{titulo: "", nombreTemplate: "cicloCombustible", noMostrarEnIndice: true}]},
-    {titulo: "Investigación y desarrollo", paginas: [{titulo: "", nombreTemplate: "investigacionYDesarrollo", noMostrarEnIndice: true}]},
-    {titulo: "Desarrollo de conocimiento", paginas: [{titulo: "", nombreTemplate: "desarrolloConocimiento", noMostrarEnIndice: true}]},
-    {titulo: "Comunicación", paginas: [{titulo: "", nombreTemplate: "comunicacion", noMostrarEnIndice: true}]},
-    {titulo: "Desafíos", paginas: [{titulo: "", nombreTemplate: "desafios", noMostrarEnIndice: true}]},
-    {titulo: "Mapa interactivo", paginas: [{titulo: "", nombreTemplate: "mapaInteractivo", noMostrarEnIndice: true}]}
+    // {titulo: "Ciclo de combustible nuclear", paginas: [{titulo: "", nombreTemplate: "cicloCombustible", noMostrarEnIndice: true}]},
+    // {titulo: "Investigación y desarrollo", paginas: [{titulo: "", nombreTemplate: "investigacionYDesarrollo", noMostrarEnIndice: true}]},
+    // {titulo: "Desarrollo de conocimiento", paginas: [{titulo: "", nombreTemplate: "desarrolloConocimiento", noMostrarEnIndice: true}]},
+    // {titulo: "Comunicación", paginas: [{titulo: "", nombreTemplate: "comunicacion", noMostrarEnIndice: true}]},
+    // {titulo: "Desafíos", paginas: [{titulo: "", nombreTemplate: "desafios", noMostrarEnIndice: true}]},
+    // {titulo: "Mapa interactivo", paginas: [{titulo: "", nombreTemplate: "mapaInteractivo", noMostrarEnIndice: true}]}
 
 ];
 
